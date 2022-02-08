0f63f339589f522775edfb10fcf0002639bc6b9b
unknown <hongyili75@gmail.com>
2022-02-08 21:27:01 +0800

    script update

---
+++ AutoUpdateBlogdata.bat
@@ -0,0 +1,7 @@
+cd public
+git init
+git remote add origin https://github.com/blanksblog/blog-data-0001.git
+git add .
+git commit -m "script update"
+git push -u origin master
+pause
---
+++ AutoUpdateGithubpage.bat
@@ -0,0 +1,6 @@
+git init
+git remote add origin https://github.com/blanksblog/blanksblog.github.io.git
+git add .
+git commit -m "script update"
+git push -u origin master
+pause
